527ecc92386cb3a7c888a88af281bc7d821ce2b9
Vicente Farías <vicfarias23@alumnos.utalca.cl>
2024-08-10 21:52:37 -0400

    COmmit ejemplo 1

--- src/main/java/com/example/prototipo/MainController.java
+++ src/main/java/com/example/prototipo/MainController.java
@@ -3,7 +3,6 @@ package com.example.prototipo;
 public class MainController {
 
     //en este metodo activara el uso de los cables del protoboard.
-
     public void toggleUsage() {
         System.out.println("Activando uso de protoboard");
     }
