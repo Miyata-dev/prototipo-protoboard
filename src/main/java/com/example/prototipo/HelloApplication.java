8e7b027fa1266e8cb192faf5b8b451d8e2949ebf
Miyata-dev <hklosteverlong@gmail.com>
2024-08-10 21:32:02 -0400

    primer commit

---
+++ src/main/java/com/example/prototipo/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.prototipo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        String css = this.getClass().getResource("styles.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
