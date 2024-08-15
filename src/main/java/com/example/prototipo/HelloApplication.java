e4756b0de2ed0a0f3bb7116c2b8c362aefc19fbe
paul <pmontecinos23@alumnos.utalca.cl>
2024-08-15 18:41:26 -0400

    Se implemento el icono de la Aplicacion que sera el de la universidad

--- src/main/java/com/example/prototipo/HelloApplication.java
+++ src/main/java/com/example/prototipo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.prototipo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("protoboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
+        Image icon = new Image(getClass().getResource("Utalca.png").toExternalForm());
+        stage.getIcons().add(icon);
         String css = this.getClass().getResource("styles.css").toExternalForm();
         scene.getStylesheets().add(css);
 
