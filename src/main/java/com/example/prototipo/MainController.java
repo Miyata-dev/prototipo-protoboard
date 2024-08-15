7c220cce995c480167f4da9fcbfad8e19c0be6ea
Vicente Farías <vicfarias23@alumnos.utalca.cl>
2024-08-15 18:58:16 -0400

    Commit: Modifique el protoboard, agregandole sus respectivos textos y componentes

--- src/main/java/com/example/prototipo/MainController.java
+++ src/main/java/com/example/prototipo/MainController.java
@@ -1,6 +1,15 @@
 package com.example.prototipo;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+
 public class MainController {
+
+    public GridPane Matriz1;
+    public GridPane Matriz2;
+    public GridPane MatrizCarga2;
+    public GridPane MatrizCarga1;
+
 
     //en este metodo activara el uso de los cables del protoboard.
     public void toggleUsage() {
