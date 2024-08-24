696efb7d220711e5ef9b43f02e2d232916cc5e7c
paul <pmontecinos23@alumnos.utalca.cl>
2024-08-23 22:04:13 -0400

    Commit de agregacion de tipo de eventos para saber si un customcircle esta siendo presionado o no esta siendo presionado

--- src/main/java/com/example/prototipo/ClickLine.java
+++ src/main/java/com/example/prototipo/ClickLine.java
@@ -1,6 +1,8 @@
 package com.example.prototipo;
 
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.control.CustomMenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -11,23 +13,52 @@ import javafx.scene.shape.Line;
 
 public class ClickLine {
     private Line line;
-    CustomCircle StartHandler, EndHandler;
+    static CustomCircle StartHandler;
+    static CustomCircle EndHandler;
 
     public static void CreateLine(AnchorPane root, GridPane gridPane){
-        root.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            boolean observador=false;
-            double x=event.getX();
-            double y=event.getY();
-            for (Node node : gridPane.getChildren()) {
-                Circle circulo = (Circle) node;
-                circulo.setOnMouseClicked(mouseEvent ->{
-                    CustomCircle startHandler = (CustomCircle) mouseEvent.getTarget();
 
-                    System.out.println("El Custom circle esta siendo presionado con la Id:" + startHandler.getId());
+
+        root.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> { //Problema con el evento Arreglar...
+            int index=0;
+            while(index < gridPane.getChildren().size()){
+                Node node= gridPane.getChildren().get(index);
+                Node node2= gridPane.getChildren().get(index);
+                Circle circulo = (Circle) node;
+                Circle circulo2 = (Circle) node2;
+                circulo.setOnMousePressed(e ->{
+                    CustomCircle circuloclick= (CustomCircle) e.getTarget();
+                    SetStartHandler(circuloclick);
+                    System.out.println("El Custom circle esta siendo presionado con la Id:" + circuloclick.getId());
+
                 });
+                index++;
             }
                 System.out.println("no le dio a un custom circle");
         });
+        root.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseEvent -> {
+            int index=0;
+            while(index < gridPane.getChildren().size()){
+
+                Node node2= gridPane.getChildren().get(index);
+                Circle circulo2 = (Circle) node2;
+                circulo2.setOnMouseReleased(event1->{
+                    CustomCircle circleFree =( CustomCircle) event1.getTarget();
+                    SetEndHandler(circleFree);
+                    System.out.println("El Custom circle esta siendo liberado con la Id:" + circleFree.getId());
+                });
+                index++;
+            }
+            System.out.println("no le dio a un custom circle");
+        });
+    }
+
+
+    public static void SetStartHandler(CustomCircle startHandler){
+        StartHandler = startHandler;
+    }
+    public static void SetEndHandler(CustomCircle endHandler){
+        EndHandler=endHandler;
     }
 
 
